69f21c961349b47bc60daf4bfd1234e2fef59896
Zebin Yang <yangzb2010@hku.hk>
2020-12-15 14:26:56 +0800

    redesign

--- liftnet/liftnet.py
+++ liftnet/liftnet.py
@@ -134,7 +134,76 @@ class BaseLIFTNet(BaseMOB, metaclass=ABCMeta):
             s1 = np.diag(inv_cov) * (x - mu)
         beta = np.average(y.reshape(-1, 1) * s1, axis=0)
         return beta.reshape([-1, 1])
+    
+    def select_split_features(self, sample_indice):
+            
+        node_x = self.x[sample_indice]
+        node_y = self.y[sample_indice]
+        n_samples, n_features = node_x.shape
 
+        if self.n_feature_search > len(self.split_features):
+            return self.split_features
+
+        feature_impurity = []
+        beta_parent = self._first_order(node_x, node_y)
+        for feature_indice in self.split_features:
+
+            current_feature = node_x[:, feature_indice]
+            sortted_indice = np.argsort(current_feature)
+            sortted_feature = current_feature[sortted_indice]
+            feature_range = sortted_feature[-1] - sortted_feature[0]
+            if feature_range < EPSILON:
+                continue
+
+            split_point = 0
+            max_deviation = 0
+            for i, _ in enumerate(sortted_indice):
+
+                if i == (n_samples - 1):
+                    continue
+
+                if ((i + 1) < self.min_samples_leaf) or ((n_samples - i - 1) < self.min_samples_leaf):
+                    continue
+
+                if sortted_feature[i + 1] <= sortted_feature[i] + EPSILON:
+                    continue
+
+                if (i + 1 - self.min_samples_leaf) < 1 / self.n_split_grid * (split_point + 1) * (n_samples - 2 * self.min_samples_leaf):
+                    continue
+
+                split_point += 1
+                left_indice = sortted_indice[:(i + 1)]
+                right_indice = sortted_indice[(i + 1):]
+                beta_left = self._first_order(node_x[left_indice], node_y[left_indice])
+                beta_right = self._first_order(node_x[right_indice], node_y[right_indice])
+                deviation = (len(left_indice) * np.linalg.norm(beta_parent - beta_left) +
+                        len(right_indice) * np.linalg.norm(beta_parent - beta_right)) / n_samples
+                if deviation > max_deviation:
+                    pos = i + 1
+                    max_deviation = deviation
+                    threshold = (sortted_feature[i] + sortted_feature[i + 1]) / 2
+            feature_impurity.append(- max_deviation)
+#             if max_deviation > 0:
+#                 left_indice = sample_indice[sortted_indice[:pos]]
+#                 right_indice = sample_indice[sortted_indice[pos:]]
+#                 left_clf = SimClassifier(reg_lambda=0, reg_gamma=1e-9, degree=self.degree,
+#                                 knot_num=self.knot_num, random_state=self.random_state)
+#                 left_clf.fit(self.x[left_indice], self.y[left_indice])
+
+#                 right_clf = SimClassifier(reg_lambda=0, reg_gamma=1e-9, degree=self.degree,
+#                                  knot_num=self.knot_num, random_state=self.random_state)
+#                 right_clf.fit(self.x[right_indice], self.y[right_indice])
+
+#                 left_impurity = self.get_loss(self.y[left_indice].ravel(), left_clf.predict_proba(self.x[left_indice])[:, 1])
+#                 right_impurity = self.get_loss(self.y[right_indice].ravel(), right_clf.predict_proba(self.x[right_indice])[:, 1])
+#                 current_impurity = (len(left_indice) * left_impurity + len(right_indice) * right_impurity) / n_samples
+#                 feature_impurity.append(current_impurity)
+#             else:
+#                 feature_impurity.append(np.inf)
+        split_feature_indices = np.argsort(feature_impurity)[:self.n_feature_search]
+        important_split_features = self.split_features[split_feature_indices]
+        return important_split_features
+        
     def visualize_one_leaf(self, node_id, folder="./results/", name="leaf_sim", save_png=False, save_eps=False):
 
         """draw one of the leaf node.
@@ -370,63 +439,6 @@ class LIFTNetRegressor(BaseLIFTNet, BaseMOBRegressor, RegressorMixin):
         node_y = self.y[sample_indice]
         n_samples, n_features = node_x.shape
 
-        feature_impurity = []
-        beta_parent = self._first_order(node_x, node_y)
-        for feature_indice in self.split_features:
-
-            current_feature = node_x[:, feature_indice]
-            sortted_indice = np.argsort(current_feature)
-            sortted_feature = current_feature[sortted_indice]
-            feature_range = sortted_feature[-1] - sortted_feature[0]
-            if feature_range < EPSILON:
-                continue
-
-            max_deviation = 0
-            split_point = 0
-            for i, _ in enumerate(sortted_indice):
-
-                if i == (n_samples - 1):
-                    continue
-
-                if ((i + 1) < self.min_samples_leaf) or ((n_samples - i - 1) < self.min_samples_leaf):
-                    continue
-
-                if sortted_feature[i + 1] <= sortted_feature[i] + EPSILON:
-                    continue
-
-                if (i + 1 - self.min_samples_leaf) < 1 / self.n_split_grid * (split_point + 1) * (n_samples - 2 * self.min_samples_leaf):
-                    continue
-
-                split_point += 1
-                left_indice = sortted_indice[:(i + 1)]
-                right_indice = sortted_indice[(i + 1):]
-                beta_left = self._first_order(node_x[left_indice], node_y[left_indice])
-                beta_right = self._first_order(node_x[right_indice], node_y[right_indice])
-                deviation = (len(left_indice) * np.linalg.norm(beta_parent - beta_left) +
-                        len(right_indice) * np.linalg.norm(beta_parent - beta_right)) / n_samples
-                if deviation > max_deviation:
-                    pos = i + 1
-                    max_deviation = deviation
-                    threshold = (sortted_feature[i] + sortted_feature[i + 1]) / 2
-
-            if max_deviation > 0:
-                left_indice = sample_indice[sortted_indice[:pos]]
-                right_indice = sample_indice[sortted_indice[pos:]]
-                left_clf = SimRegressor(reg_lambda=0, reg_gamma=1e-9, degree=self.degree,
-                                knot_num=self.knot_num, random_state=self.random_state)
-                left_clf.fit(self.x[left_indice], self.y[left_indice])
-
-                right_clf = SimRegressor(reg_lambda=0, reg_gamma=1e-9, degree=self.degree,
-                                 knot_num=self.knot_num, random_state=self.random_state)
-                right_clf.fit(self.x[right_indice], self.y[right_indice])
-
-                left_impurity = self.get_loss(self.y[left_indice].ravel(), left_clf.predict(self.x[left_indice]))
-                right_impurity = self.get_loss(self.y[right_indice].ravel(), right_clf.predict(self.x[right_indice]))
-                current_impurity = (len(left_indice) * left_impurity + len(right_indice) * right_impurity) / n_samples
-                feature_impurity.append(current_impurity)
-            else:
-                feature_impurity.append(np.inf)
-
         best_feature = None
         best_position = None
         best_threshold = None
@@ -435,8 +447,8 @@ class LIFTNetRegressor(BaseLIFTNet, BaseMOBRegressor, RegressorMixin):
         best_impurity = np.inf
         best_left_impurity = np.inf
         best_right_impurity = np.inf
-        split_features = np.argsort(feature_impurity)[:self.n_feature_search]
-        for feature_indice in split_features:
+        important_split_features = self.select_features(sample_indice)
+        for feature_indice in important_split_features:
 
             current_feature = node_x[:, feature_indice]
             sortted_indice = np.argsort(current_feature)
@@ -543,63 +555,6 @@ class LIFTNetClassifier(BaseLIFTNet, BaseMOBClassifier, ClassifierMixin):
         node_y = self.y[sample_indice]
         n_samples, n_features = node_x.shape
 
-        feature_impurity = []
-        beta_parent = self._first_order(node_x, node_y)
-        for feature_indice in self.split_features:
-
-            current_feature = node_x[:, feature_indice]
-            sortted_indice = np.argsort(current_feature)
-            sortted_feature = current_feature[sortted_indice]
-            feature_range = sortted_feature[-1] - sortted_feature[0]
-            if feature_range < EPSILON:
-                continue
-
-            split_point = 0
-            max_deviation = 0
-            for i, _ in enumerate(sortted_indice):
-
-                if i == (n_samples - 1):
-                    continue
-
-                if ((i + 1) < self.min_samples_leaf) or ((n_samples - i - 1) < self.min_samples_leaf):
-                    continue
-
-                if sortted_feature[i + 1] <= sortted_feature[i] + EPSILON:
-                    continue
-
-                if (i + 1 - self.min_samples_leaf) < 1 / self.n_split_grid * (split_point + 1) * (n_samples - 2 * self.min_samples_leaf):
-                    continue
-
-                split_point += 1
-                left_indice = sortted_indice[:(i + 1)]
-                right_indice = sortted_indice[(i + 1):]
-                beta_left = self._first_order(node_x[left_indice], node_y[left_indice])
-                beta_right = self._first_order(node_x[right_indice], node_y[right_indice])
-                deviation = (len(left_indice) * np.linalg.norm(beta_parent - beta_left) +
-                        len(right_indice) * np.linalg.norm(beta_parent - beta_right)) / n_samples
-                if deviation > max_deviation:
-                    pos = i + 1
-                    max_deviation = deviation
-                    threshold = (sortted_feature[i] + sortted_feature[i + 1]) / 2
-            feature_impurity.append(- max_deviation)
-#             if max_deviation > 0:
-#                 left_indice = sample_indice[sortted_indice[:pos]]
-#                 right_indice = sample_indice[sortted_indice[pos:]]
-#                 left_clf = SimClassifier(reg_lambda=0, reg_gamma=1e-9, degree=self.degree,
-#                                 knot_num=self.knot_num, random_state=self.random_state)
-#                 left_clf.fit(self.x[left_indice], self.y[left_indice])
-
-#                 right_clf = SimClassifier(reg_lambda=0, reg_gamma=1e-9, degree=self.degree,
-#                                  knot_num=self.knot_num, random_state=self.random_state)
-#                 right_clf.fit(self.x[right_indice], self.y[right_indice])
-
-#                 left_impurity = self.get_loss(self.y[left_indice].ravel(), left_clf.predict_proba(self.x[left_indice])[:, 1])
-#                 right_impurity = self.get_loss(self.y[right_indice].ravel(), right_clf.predict_proba(self.x[right_indice])[:, 1])
-#                 current_impurity = (len(left_indice) * left_impurity + len(right_indice) * right_impurity) / n_samples
-#                 feature_impurity.append(current_impurity)
-#             else:
-#                 feature_impurity.append(np.inf)
-
         best_feature = None
         best_position = None
         best_threshold = None
@@ -608,8 +563,8 @@ class LIFTNetClassifier(BaseLIFTNet, BaseMOBClassifier, ClassifierMixin):
         best_impurity = np.inf
         best_left_impurity = np.inf
         best_right_impurity = np.inf
-        split_features = np.argsort(feature_impurity)[:self.n_feature_search]
-        for feature_indice in split_features:
+        important_split_features = self.select_features(sample_indice)
+        for feature_indice in important_split_features:
 
             current_feature = node_x[:, feature_indice]
             sortted_indice = np.argsort(current_feature)
