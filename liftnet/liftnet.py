fa05acc0df5806aaa508206bca243a18305b2652
Zebin Yang <yangzb2010@hku.hk>
2020-12-16 13:51:45 +0800

    first_order normalize x's variance before truncation

--- liftnet/liftnet.py
+++ liftnet/liftnet.py
@@ -26,12 +26,11 @@ class BaseLIFTNet(BaseMOB, metaclass=ABCMeta):
         Base LIFTNet class for classification and regression.
      """
 
-    @abstractmethod
     def __init__(self, max_depth=2, min_samples_leaf=10, min_impurity_decrease=0.0001,
                  n_split_grid=10, split_features=None, n_feature_search=1,
                  degree=3, knot_num=5, reg_lambda=0.1, reg_gamma=0.1, random_state=0):
 
-        super(BaseMOB, self).__init__(max_depth=max_depth,
+        super(BaseLIFTNet, self).__init__(max_depth=max_depth,
                                  min_samples_leaf=min_samples_leaf,
                                  min_impurity_decrease=min_impurity_decrease,
                                  split_features=split_features,
