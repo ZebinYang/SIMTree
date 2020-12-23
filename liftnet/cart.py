fix the impurity calculation error of constant nodes

liftnet/cart.py
@@ -113,7 +113,7 @@ class CARTClassifier(MoBTreeClassifier, ClassifierMixin):
     def build_leaf(self, sample_indice):
 
         best_estimator = None
-        predict_func = lambda x: np.ones(len(sample_indice)) * self.y[sample_indice].mean()
+        predict_func = lambda x: np.ones(x.shape[0]) * self.y[sample_indice].mean()
         best_impurity = self.get_loss(self.y[sample_indice], predict_func(self.x[sample_indice]))
         return predict_func, best_estimator, best_impurity
 

liftnet/customtree.py
@@ -91,7 +91,7 @@ class CustomMobTreeClassifier(MoBTreeClassifier, RegressorMixin):
 
         if (self.y[sample_indice].std() == 0) | (self.y[sample_indice].sum() < 5) | ((1 - self.y[sample_indice]).sum() < 5):
             best_estimator = None
-            predict_func = lambda x: np.ones(len(sample_indice)) * self.y[sample_indice].mean()
+            predict_func = lambda x: np.ones(x.shape[0]) * self.y[sample_indice].mean()
             best_impurity = self.get_loss(self.y[sample_indice], predict_func(self.x[sample_indice]))
         else:
             grid = GridSearchCV(self.base_estimator, param_grid=self.param_dict,

liftnet/glmtree.py
@@ -78,7 +78,7 @@ class GLMTreeClassifier(MoBTreeClassifier, ClassifierMixin):
 
         if (self.y[sample_indice].std() == 0) | (self.y[sample_indice].sum() < 5) | ((1 - self.y[sample_indice]).sum() < 5):
             best_estimator = None
-            predict_func = lambda x: np.ones(len(sample_indice)) * self.y[sample_indice].mean()
+            predict_func = lambda x: np.ones(x.shape[0]) * self.y[sample_indice].mean()
             best_impurity = self.get_loss(self.y[sample_indice], predict_func(self.x[sample_indice]))
         else:
             best_estimator = LogisticRegressionCV(Cs=self.reg_lambda, penalty="l1", solver="liblinear",

liftnet/liftnet.py
@@ -356,7 +356,7 @@ class LIFTNetClassifier(LIFTNet, MoBTreeClassifier, ClassifierMixin):
 
         if (self.y[sample_indice].std() == 0) | (self.y[sample_indice].sum() < 5) | ((1 - self.y[sample_indice]).sum() < 5):
             best_estimator = None
-            predict_func = lambda x: np.ones(len(sample_indice)) * self.y[sample_indice].mean()
+            predict_func = lambda x: np.ones(x.shape[0]) * self.y[sample_indice].mean()
             best_impurity = self.get_loss(self.y[sample_indice], predict_func(self.x[sample_indice]))
         else:
             base = SimClassifier(reg_gamma=self.reg_gamma, degree=self.degree,