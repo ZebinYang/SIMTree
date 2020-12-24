fcc367d0f81a16e8aef0b851959ac21044fc3594
Zebin Yang <yangzb2010@hku.hk>
2020-12-24 16:48:41 +0800

    rm clip predict

--- liftnet/customtree.py
+++ liftnet/customtree.py
@@ -45,7 +45,7 @@ class CustomMobTreeRegressor(MoBTreeRegressor, RegressorMixin):
         grid = GridSearchCV(self.base_estimator, param_grid=self.param_dict,
                       scoring={"mse": make_scorer(mean_squared_error, greater_is_better=False)},
                       cv=5, refit="mse", n_jobs=1, error_score=np.nan)
-        grid.fit(nx, self.y[sample_indice].ravel())
+        grid.fit(self.x[sample_indice], self.y[sample_indice].ravel())
         best_estimator = grid.best_estimator_
         predict_func = lambda x: best_estimator.predict(x)
         best_impurity = self.get_loss(self.y[sample_indice], best_estimator.predict(x))
@@ -88,7 +88,7 @@ class CustomMobTreeClassifier(MoBTreeClassifier, RegressorMixin):
             grid = GridSearchCV(self.base_estimator, param_grid=self.param_dict,
                           scoring={"auc": make_scorer(roc_auc_score, needs_proba=True)},
                           cv=5, refit="auc", n_jobs=1, error_score=np.nan)
-            grid.fit(nx, self.y[sample_indice].ravel())
+            grid.fit(self.x[sample_indice], self.y[sample_indice].ravel())
             best_estimator = grid.best_estimator_
             predict_func = lambda x: best_estimator.predict_proba(x)[:, 1]
             best_impurity = self.get_loss(self.y[sample_indice], best_estimator.predict_proba(x)[:, 1])
